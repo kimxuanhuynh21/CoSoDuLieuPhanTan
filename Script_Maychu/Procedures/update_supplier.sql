8a16e91a578939df5c84f05a5c21bdda24d15dbf
Nhieu <vannhieunguyen1994@gmail.com>
2017-05-15 12:50:52 +0700

    Add sample script for update

--- Script_Maychu/Procedures/update_supplier.sql
+++ Script_Maychu/Procedures/update_supplier.sql
@@ -10,25 +10,52 @@ GO
 -- =============================================
 CREATE PROCEDURE update_supplier
 	@supplierId nvarchar(11)
-	,@name nvarchar(200)
-	,@addressSupplier nvarchar(200)
-	,@phone varchar(100)
-	,@email varchar(100)
-	,@note nvarchar(255)
+	,@name nvarchar(200) = NULL
+	,@addressSupplier nvarchar(200) = NULL
+	,@phone varchar(100) = NULL
+	,@email varchar(100) = NULL
+	,@note nvarchar(255) = NULL
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 	
-	UPDATE [ThoiTrang_CSDL].[dbo].[supplier]
-	   SET [name] = @name
-		  ,[addressSupplier] = @addressSupplier
-		  ,[phone] = @phone
-		  ,[email] = @email
-		  ,[dateChanged] = GETDATE()
-		  ,[note] = @note
-	 WHERE [supplierId] = @supplierId 
+    DECLARE @SQLQuery AS nvarchar(4000);
+    DECLARE @ParamDefinition AS nvarchar(2000);
+	SET @SQLQuery = 'UPDATE [ThoiTrang_CSDL].[dbo].[supplier] SET [dateChanged] = GETDATE()';
+    If @name IS NOT NULL 
+         SET @SQLQuery = @SQLQuery + ', [name] = @name';
+    If @addressSupplier IS NOT NULL 
+         SET @SQLQuery = @SQLQuery + ', [addressSupplier] = @addressSupplier';
+    If @phone IS NOT NULL 
+         SET @SQLQuery = @SQLQuery + ', [phone] = @phone';
+    If @email IS NOT NULL 
+         SET @SQLQuery = @SQLQuery + ', [email] = @email';
+    If @note IS NOT NULL 
+         SET @SQLQuery = @SQLQuery + ', [note] = @note'
+	SET @SQLQuery = @SQLQuery + ' WHERE [supplierId] = @supplierId';
+	
+	SET @ParamDefinition = '@supplierId nvarchar(11)
+	,@name nvarchar(200)
+	,@addressSupplier nvarchar(200)
+	,@phone varchar(100)
+	,@email varchar(100)';
+	PRINT @SQLQuery;
+    Execute sp_Executesql @SQLQuery 
+                ,@ParamDefinition
+				,@supplierId
+				,@name
+				,@addressSupplier
+				,@phone
+				,@email;
+
+    If @@ERROR <> 0 GoTo ErrorHandler
+    Set NoCount OFF
+    Return(0)
+  
+ErrorHandler:
+    Return(@@ERROR) 
 END
 GO
 
